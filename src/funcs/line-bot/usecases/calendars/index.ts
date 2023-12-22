cf20e7a971b4cab7aa14fee6368e56c59e077042
jam-naka <kosukengineer@gmail.com>
2023-12-22 23:01:42 +0900

    get calendar process

--- src/funcs/line-bot/usecases/calendars/index.ts
+++ src/funcs/line-bot/usecases/calendars/index.ts
@@ -1,8 +1,8 @@
-import { google } from 'googleapis';
+import { google, calendar_v3 } from 'googleapis';
 import { GoogleAuth } from 'google-auth-library';
 
 const auth = new GoogleAuth({
-    keyFile: "../../../../credentials.json",
+    keyFile: "service-account-key.json",
     scopes: "https://www.googleapis.com/auth/calendar"
 });
 
@@ -10,6 +10,26 @@ async function getAccessToken() {
     try {
         const client = await auth.getClient();
         const accessToken = await client.getAccessToken();
+
+        const calendar: calendar_v3.Calendar = google.calendar({ version: 'v3', auth: client });
+        // イベントを取得する期間を設定
+        const start = '2023-01-01T00:00:00.000Z'; // 開始日時（ISOフォーマット）
+        const end = '2023-01-31T23:59:59.999Z';   // 終了日時（ISOフォーマット）
+    
+        // イベントのリストを取得
+        const response = await calendar.events.list({
+          calendarId: 'primary',
+          timeMin: start,
+          timeMax: end,
+          singleEvents: true,
+          orderBy: 'startTime',
+        });
+    
+        const events = response.data.items;
+        if (!events || events.length === 0) {
+          console.log('No upcoming events found.');
+          return;
+        }
         return accessToken;
     } catch (error) {
         console.error('Error getting access token:', error);
