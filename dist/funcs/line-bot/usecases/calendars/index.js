d56c5fa60149b48957cf5423bc532758738f205a
shoma-endo <nissin20991@gmail.com>
2023-12-24 23:22:51 +0900

    fix: 不要箇所削除

---
+++ dist/funcs/line-bot/usecases/calendars/index.js
@@ -0,0 +1,42 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getAccessToken = void 0;
+const googleapis_1 = require("googleapis");
+const google_auth_library_1 = require("google-auth-library");
+const auth = new google_auth_library_1.GoogleAuth({
+    keyFile: "service-account-key.json",
+    scopes: "https://www.googleapis.com/auth/calendar"
+});
+async function getAccessToken() {
+    try {
+        const client = await auth.getClient();
+        const accessToken = await client.getAccessToken();
+        const calendar = googleapis_1.google.calendar({ version: 'v3', auth: client });
+        const start = '2023-12-20T00:00:00.000Z';
+        const end = '2023-12-31T23:59:59.999Z';
+        const response = await calendar.events.list({
+            calendarId: 'primary',
+            timeMin: start,
+            timeMax: end,
+            singleEvents: true,
+            orderBy: 'startTime',
+        });
+        calendar.calendarList.list({}, (err, res) => {
+            const calendars = res.data.items;
+            console.log(calendars);
+        });
+        const calendarEntry = response.data;
+        console.log(JSON.stringify(calendarEntry, null, 2));
+        const events = response.data.items;
+        if (!events || events.length === 0) {
+            console.log('No upcoming events found.');
+            return;
+        }
+        return accessToken;
+    }
+    catch (error) {
+        console.error('Error getting access token:', error);
+        throw error;
+    }
+}
+exports.getAccessToken = getAccessToken;
--- src/funcs/line-bot/index.ts
+++ src/funcs/line-bot/index.ts
@@ -4,7 +4,7 @@ import express from 'express'
 import { lineMiddlewareConfig } from '~/clients/line.client'
 
 import { usecases } from './usecases'
-import { getAccessToken } from './usecases/calendars';
+// import { getAccessToken } from './usecases/calendars';
 
 const app = express()
 
@@ -12,15 +12,7 @@ app.post('/webhook', middleware(lineMiddlewareConfig), (req, res) => {
   Promise
     .all(req.body.events.map(usecases))
     .then((result) => {
-      // アプリケーションの初期化時にアクセストークンを取得
-      let text = '';
-      getAccessToken().then(accessToken => {
-        text = accessToken as any;
-        // ここで accessToken を使用するロジックを実装
-      }).catch(error => {
-        console.error('Error during access token retrieval:', error);
-      });
-      res.json(text)
+      res.json(result)
       res.status(200).end()
     })
     .catch((err) => {
