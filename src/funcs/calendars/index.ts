8e5a9e86705078fbb4cd1247193dd8d6fcd82f43
jam-naka <kosukengineer@gmail.com>
2023-12-31 12:01:40 +0900

    fix ESmodule

--- src/funcs/calendars/index.ts
+++ src/funcs/calendars/index.ts
@@ -3,32 +3,28 @@ import { Credentials } from 'google-auth-library';
 
 import { GOOGLE_OAUTH_CLIENT_ID, GOOGLE_OAUTH_SECRET, APP_URL } from '../../utils/secrets'
 
-let userTokens: Credentials = {};
+export let userTokens: Credentials = {};
 
 const OAuth2 = google.auth.OAuth2;
 
-const oauth2Client = new OAuth2(
-    GOOGLE_OAUTH_CLIENT_ID,
-    GOOGLE_OAUTH_SECRET,
-    APP_URL + "/callback"
+export const oauth2Client = new OAuth2(
+  GOOGLE_OAUTH_CLIENT_ID,
+  GOOGLE_OAUTH_SECRET,
+  APP_URL + "/callback"
 );
 
-const authUrl = oauth2Client.generateAuthUrl({
+export const authUrl = oauth2Client.generateAuthUrl({
   access_type: 'offline',
   scope: 'https://www.googleapis.com/auth/calendar', // 必要なスコープを指定
 });
 
-async function saveUserTokens(code: string) : Promise<void> {
+export const saveUserTokens = async (code: string) : Promise<void> => {
   const response = await oauth2Client.getToken(code);
   const tokens: Credentials = response.tokens;
   userTokens = tokens;
   console.log(userTokens);
 }
 
-function deleteUserTokens() : void {
+export const deleteUserTokens = (): void => {
   userTokens = {};
 }
-
-
-
-export { userTokens, oauth2Client, authUrl, saveUserTokens, deleteUserTokens };
