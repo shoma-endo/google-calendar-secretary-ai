e9facef7a169f9dd9baffdd4dda570bf1610ce8d
jam-naka <kosukengineer@gmail.com>
2023-12-31 12:04:22 +0900

    fix js

--- dist/funcs/calendars/index.js
+++ dist/funcs/calendars/index.js
@@ -3,24 +3,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteUserTokens = exports.saveUserTokens = exports.authUrl = exports.oauth2Client = exports.userTokens = void 0;
 const googleapis_1 = require("googleapis");
 const secrets_1 = require("../../utils/secrets");
-let userTokens = {};
-exports.userTokens = userTokens;
+exports.userTokens = {};
 const OAuth2 = googleapis_1.google.auth.OAuth2;
-const oauth2Client = new OAuth2(secrets_1.GOOGLE_OAUTH_CLIENT_ID, secrets_1.GOOGLE_OAUTH_SECRET, secrets_1.APP_URL + "/callback");
-exports.oauth2Client = oauth2Client;
-const authUrl = oauth2Client.generateAuthUrl({
+exports.oauth2Client = new OAuth2(secrets_1.GOOGLE_OAUTH_CLIENT_ID, secrets_1.GOOGLE_OAUTH_SECRET, secrets_1.APP_URL + "/callback");
+exports.authUrl = exports.oauth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: 'https://www.googleapis.com/auth/calendar',
 });
-exports.authUrl = authUrl;
-async function saveUserTokens(code) {
-    const response = await oauth2Client.getToken(code);
+const saveUserTokens = async (code) => {
+    const response = await exports.oauth2Client.getToken(code);
     const tokens = response.tokens;
-    exports.userTokens = userTokens = tokens;
-    console.log(userTokens);
-}
+    exports.userTokens = tokens;
+    console.log(exports.userTokens);
+};
 exports.saveUserTokens = saveUserTokens;
-function deleteUserTokens() {
-    exports.userTokens = userTokens = {};
-}
+const deleteUserTokens = () => {
+    exports.userTokens = {};
+};
 exports.deleteUserTokens = deleteUserTokens;
